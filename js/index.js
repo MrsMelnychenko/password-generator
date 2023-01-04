d53ecea02cc7d6fda55963f2eed4f58e3b2dc073
Mariana <melnychenko.mariana@gmail.com>
2023-01-04 12:49:01 +0200

    added function to display changes based on scale

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
-// const rgxUpper = //
-// const rgxLower = //
-// const rgxNum = //
-// const rgxChar = //
+let scale = document.querySelector(".range");
+const passwdBaseNum = '0123456789';
+const passwdBaseAZ = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const passwdBaseaz ='abcdefghijklmnopqrstuvwxyz';
+const passwdBaseSpec = '!"#$%&()*+,-./:;<=?@[]\^_`{|}~';
+let password = '';
+
+//Function to display password length based on user`s scale
+passwdLength = document.querySelector('.char-password');
+passwdLength.innerHTML = scale.value;
+    
+scale.addEventListener('input', function () {
+  passwdLength.innerHTML = scale.value;
+}, false);
+
