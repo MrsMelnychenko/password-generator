863a3bfac410d328307b179df84cf7ecfc20d362
Mariana <melnychenko.mariana@gmail.com>
2023-01-04 16:08:26 +0200

    added func to generate password for each checkbox

--- js/index.js
+++ js/index.js
@@ -3,7 +3,13 @@ const passwdBaseNum = '0123456789';
 const passwdBaseAZ = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const passwdBaseaz ='abcdefghijklmnopqrstuvwxyz';
 const passwdBaseSpec = '!"#$%&()*+,-./:;<=?@[]\^_`{|}~';
-let password = '';
+
+//Checkbox options
+const Upper = document.querySelector(".Uppercase");
+const Lower = document.querySelector(".Lowercase");
+const Nums = document.querySelector(".Numbers");
+const Symbols = document.querySelector(".Special-characters");
+
 
 //Function to display password length based on user`s scale
 passwdLength = document.querySelector('.char-password');
@@ -13,3 +19,33 @@ scale.addEventListener('input', function () {
   passwdLength.innerHTML = scale.value;
 }, false);
 
+// Function to generate password
+function generatePasswd() {
+    let password = '';
+    let passwdBase = '';
+    // debugger;
+    for (let i = 0; i <= scale.value; i++) {
+        //   IFs to set a string to generate password from
+        if (Upper.checked === true) {
+            passwdBase = passwdBaseAZ;
+        }
+        if (Lower.checked === true) {
+            passwdBase = passwdBaseaz;
+        }
+        if (Nums.checked === true) {
+            passwdBase = passwdBaseNum;
+        }
+        if (Symbols.checked === true) {
+            passwdBase = passwdBaseSpec;
+        }
+      
+        let random = Math.floor(Math.random() * passwdBase.length);
+        password += passwdBase.substring(random, random + 1);
+        
+    }
+    document.querySelector(".gen-passwd").value = password;
+
+}
+
+const generator = document.querySelector(".generate-btn");
+generator.addEventListener("click", generatePasswd);
